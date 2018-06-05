eed56a5e07197d3031b1d5794e95020c2530ee99
maolin <772984968@qq.com>
2018-06-05 16:24:25 +0800

    后台修改

---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+@section('_meta')
+@include('layouts._meta')
+@endsection
+@section('title','首页')
+@section('content')
+
+
+@stop
--- routes/admin/web.php
+++ routes/admin/web.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-//
-
 Route::group(['prefix'=>'/admin','namespace'=>'Admin'],function(){
        //登陆逻辑
        require_once __DIR__.'/login.php';
@@ -23,15 +10,7 @@ Route::group(['prefix'=>'/admin','namespace'=>'Admin'],function(){
            Route::get('index','AdminController@index')->name('index');
            //后台欢迎页
            Route::get('welcome','AdminController@welcome')->name('welcome');
-            //等级管理
-           Route::resource('level','LevelController');
-           //意见反馈
-           Route::resource('feedback','FeedbackController');
-           //用户管理
-           Route::resource('user','UserController');
-           //成语管理
-           Route::resource('idiom','IdiomController');
-           //活动管理
-           Route::resource('activity','ActivityController');
+            //类型管理
+           Route::resource('category','CategoryController');
        });
 });
